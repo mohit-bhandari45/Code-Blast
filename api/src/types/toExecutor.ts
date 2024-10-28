41db4c009b03734854ea1c27de7b6572f1a4bb79
tivarii <adarsh.t@w3dev.email>
2024-10-28 18:39:34 +0530

    adding redismanager and adding the route code-executor

---
+++ api/src/types/toExecutor.ts
@@ -0,0 +1,13 @@
+export type testCaseInput = {
+    data_input: string;
+    lang: string;
+    question_id: string;
+    typed_code: string;
+    submission_id: string;
+};
+export type submitInput = {
+    lang: string;
+    question_id: string;
+    typed_code: string;
+    submission_id: string;
+};
