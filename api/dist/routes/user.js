6c1d3ade81164097445750236e2db96a39468eaf
mohit-javascript <mohit.b@w3dev.email>
2024-11-02 09:00:34 +0530

    added the authentication and eslint problems

--- api/dist/routes/user.js
+++ api/dist/routes/user.js
@@ -1,9 +1,6 @@
 import express from "express";
 import { handleLogin, handleSignUp } from "../controllers/users.js";
-
 const router = express.Router();
-
 router.post("/signup", handleSignUp);
 router.post("/login", handleLogin);
-
 export default router;
--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,16 +1,33 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
 
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
     ignores: ["node_modules/", "dist/", "build/"],
   },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: "readonly", // Declare 'process' as a global variable
+      },
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettier,
+    },
     rules: {
       semi: ["error", "always"],
       quotes: ["error", "double"],
@@ -19,10 +36,7 @@ export default [
         { argsIgnorePattern: "^_" },
       ],
       "no-unused-vars": "off",
-      "prettier/prettier": "error", // Enable Prettier rules as ESLint errors
+      "prettier/prettier": "error",
     },
-  },
-  {
-    plugins: ["prettier"], // Add prettier as a plugin
   },
 ];
