35530820b37d4e0af92cf3b5600bee35e1ed431a
mohit-bhandari45 <mohitbhandari852@gmail.com>
2024-11-18 19:58:46 +0530

    changes agina

--- api/src/types/types.ts
+++ api/src/types/types.ts
@@ -1,6 +1,8 @@
+/* eslint-disable prettier/prettier */
 import JWT from "jsonwebtoken";
 
 export default interface CustomJwtPayload extends JWT.JwtPayload {
   id: string;
   email: string;
+// eslint-disable-next-line semi
 }
