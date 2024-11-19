795832ae53989e5fa387fec6556947dff7f4044a
mohit-bhandari45 <mohitbhandari852@gmail.com>
2024-11-19 20:47:16 +0530

    linting fix

--- api/src/types/types.ts
+++ api/src/types/types.ts
@@ -1,6 +1,8 @@
+/* eslint-disable semi */
 import JWT from "jsonwebtoken";
 
 export default interface CustomJwtPayload extends JWT.JwtPayload {
   id: string;
   email: string;
+  // eslint-disable-next-line prettier/prettier
 }
